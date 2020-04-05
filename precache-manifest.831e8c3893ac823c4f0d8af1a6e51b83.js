8d34217647ece34e77c42b44963a12cf5f03ffcf
Yehuda Kremer <yehudakremer@gmail.com>
2020-04-05 23:44:14 +0300

    Updates

---
+++ precache-manifest.831e8c3893ac823c4f0d8af1a6e51b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7ced7f4622c07ebf0947481496c7e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9042a3184d277e06d0e7",
+    "url": "/static/css/main.28fddd66.chunk.css"
+  },
+  {
+    "revision": "b4408537817046d2363f",
+    "url": "/static/js/2.ad91bb1a.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.ad91bb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9042a3184d277e06d0e7",
+    "url": "/static/js/main.ed3fc446.chunk.js"
+  },
+  {
+    "revision": "52e4cdf748d8b3d1cd4e",
+    "url": "/static/js/runtime-main.9fe15c4c.js"
+  }
+]);
